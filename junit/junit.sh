473b65016fa7775234e1813f01fe87a2d1997002
root <root@fadfdefefbaefddfddddfcdeaeddec-0.fadfdefefbaefddfddddfcdeaeddec.che.svc.cluster.local>
2023-03-27 04:45:21 +0000

    Backup Branch before deleting

---
+++ junit/junit.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cp -r /home/coder/project/workspace/junit/test /home/coder/project/workspace/jspservlet-app-stringmania/test/;
+cd /home/coder/project/workspace/jspservlet-app-stringmania/;
+mvn clean test
